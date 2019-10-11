c7bea29527ec5ff5759970dd7b9b599d679be0a0
jzwart <jzwart@usgs.gov>
2019-10-11 10:40:34 -0400

    updating start / stop

--- 4_model/src/run_sntemp.R
+++ 4_model/src/run_sntemp.R
@@ -1,5 +1,4 @@
 
-#'
 #'run SNTemp model executable
 #'
 #'@param start start date for SNTemp model
@@ -120,18 +119,32 @@ set_start_stop = function(start, stop, control_file = 'delaware.control'){
   writeLines(text = ctrl, con = file.path('20190913_Delaware_streamtemp/control', control_file))
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
 starts = seq.Date(from = as.Date('2000-06-01'), to = as.Date('2000-08-10'), by = 'days')
-stops = starts + 1
+stops = starts
 
 model_output_file = '20190913_Delaware_streamtemp/output/seg_tave_water.csv'
 model_fabric_file = '20190913_Delaware_streamtemp/GIS/Segments_subset.shp'
 
 out = data.frame()
 
+spinup_days = 730
+
 for(i in 1:length(starts)){
   if(i == 1){
     # run spinup period if first timestep
-    run_sntemp(start = starts[i], stop = stops[i], spinup = T, spinup_days = 730, restart = T)
+    run_sntemp(start = starts[i], stop = stops[i], spinup = T, spinup_days = spinup_days, restart = T)
   }else{
     run_sntemp(start = starts[i], stop = stops[i], spinup = F, restart = T)
   }
@@ -139,16 +152,80 @@ for(i in 1:length(starts)){
   stream_temp = get_modeled_temperature(model_output_file = model_output_file,
                                         model_fabric_file = model_fabric_file)
 
+  stream_discharge = get_modeled_discharge(model_output_file = '20190913_Delaware_streamtemp/output/seg_outflow.csv',
+                                           model_fabric_file = model_fabric_file)
+
+  stream_temp = left_join(stream_temp, stream_discharge, by = c('seg_id_nat', 'model_idx', 'date'))
+
   out = rbind(out, dplyr::filter(stream_temp, as.character(date) == as.character(stops[i])))
 }
 
 
-seg = dplyr::filter(out, model_idx == '20')
-plot(seg$water_temp, type = 'l')
+# seg = dplyr::filter(out, model_idx == '2')
+# plot(seg$water_temp, type = 'l')
 
 
 # running model w/o restarting to compare to restarts
-run_sntemp(start = starts[1]-730-1, stop = stops[length(stops)], spinup = F, spinup_days = 730, restart = F)
+run_sntemp(start = starts[1]-spinup_days-1, stop = stops[length(stops)], spinup = F, restart = F)
+no_restart = get_modeled_temperature(model_output_file = model_output_file,
+                                     model_fabric_file = model_fabric_file) %>%
+  dplyr::filter(date %in% out$date)
 
+no_restart_discharge = get_modeled_discharge(model_output_file = '20190913_Delaware_streamtemp/output/seg_outflow.csv',
+                                         model_fabric_file = model_fabric_file)
+
+no_restart = left_join(no_restart, no_restart_discharge, by = c('seg_id_nat', 'model_idx', 'date'))
+
+
+
+model_seg = '200'
+seg = dplyr::filter(out, model_idx == model_seg)
+seg_no_restart = dplyr::filter(no_restart, model_idx == model_seg)
+
+plot(seg$water_temp ~ seg$date, type = 'l')
+lines(seg_no_restart$water_temp ~ seg_no_restart$date, col ='red')
+
+all_out = left_join(out, no_restart, by = c('model_idx', 'date'), suffix = c('', '_no_restart'))
+plot(all_out$water_temp ~ all_out$water_temp_no_restart, ylim = c(0, 60), xlim = c(0, 60))
+abline(0,1)
+
+ggplot(all_out, aes(y = water_temp, x = water_temp_no_restart, group = model_idx)) +
+  geom_smooth(method = 'lm', se = F, fullrange = F) +
+  geom_abline(slope = 1, intercept = 0, linetype = 'dashed', size = 2, col = 'red') +
+  geom_point(alpha = .1) +
+  theme_classic() +
+  xlim(c(0,60)) +
+  ylim(c(0,60)) +
+  ylab('Water Temp With Restarts') +
+  xlab('Water Temp Baseline Model Run')
+
+
+ggplot(all_out, aes(y = discharge, x = discharge_no_restart, group = model_idx)) +
+  geom_smooth(method = 'lm', se = F, fullrange = F) +
+  geom_abline(slope = 1, intercept = 0, linetype = 'dashed', size = 2, col = 'red') +
+  geom_point(alpha = .1) +
+  theme_classic() +
+  ylab('Discharge With Restarts') +
+  xlab('Discharge Baseline Model Run')
+
+ggplot(all_out, aes(y = discharge, x = discharge_no_restart, group = model_idx)) +
+  geom_smooth(method = 'lm', se = F, fullrange = F) +
+  geom_abline(slope = 1, intercept = 0, linetype = 'dashed', size = 2, col = 'red') +
+  geom_point(alpha = .1) +
+  theme_classic() +
+  ylab('Discharge With Restarts') +
+  xlab('Discharge Baseline Model Run') +
+  ylim(c(0,1000)) +
+  xlim(c(0,1000))
+
+Metrics::rmse(all_out$discharge, all_out$discharge_no_restart)
+Metrics::rmse(all_out$water_temp, all_out$water_temp_no_restart)
+
+
+plot(seg$discharge ~ seg$date, type = 'l', ylab= 'Discharge' ,xlab ='' )
+lines(seg_no_restart$discharge ~ seg_no_restart$date, col ='red')
+
+plot(seg$water_temp ~ seg$date, type = 'l')
+lines(seg_no_restart$water_temp ~ seg_no_restart$date, col ='red')
 
 
