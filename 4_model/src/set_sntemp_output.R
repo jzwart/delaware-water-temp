7e985da7ed13ee7b8df8fe6b46d616ca1b62f690
jzwart <jzwart@usgs.gov>
2019-10-15 12:49:54 -0400

    rebuild of output

--- 4_model/src/set_sntemp_output.R
+++ 4_model/src/set_sntemp_output.R
@@ -13,11 +13,15 @@ set_sntemp_output = function(output_names, control_file = 'delaware.control'){
 
     new_output_loc_start = grep('aniOutVar_names', new_ctrl) + 3
 
+    new_ctrl[grep('aniOutVar_names', new_ctrl) + 1] = as.character(length(output_names$sntemp_output))
+
     new_ctrl = c(new_ctrl[1:(new_output_loc_start-1)], output_names$sntemp_output, new_ctrl[new_output_loc_start:length(new_ctrl)])
   }else{
     new_ctrl = ctrl[-c(output_loc_start:output_loc_end)] # get rid of old output variables
 
     new_output_loc_start = grep('aniOutVar_names', new_ctrl) + 3
+
+    new_ctrl[grep('aniOutVar_names', new_ctrl) + 1] = as.character(length(output_names$sntemp_output))
 
     new_ctrl = c(new_ctrl[1:(new_output_loc_start-1)], output_names$sntemp_output, new_ctrl[new_output_loc_start:length(new_ctrl)])
   }
