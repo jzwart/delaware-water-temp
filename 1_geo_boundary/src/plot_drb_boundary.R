8749464fd5ed02fe89d9cd8b69c5d1d2acda43c6
jzwart <jzwart@usgs.gov>
2019-11-15 11:32:32 -0500

    minor plot changes for one-off plots

--- 1_geo_boundary/src/plot_drb_boundary.R
+++ 1_geo_boundary/src/plot_drb_boundary.R
@@ -23,7 +23,7 @@ m = sf::st_as_sf(maps::map(database = 'state',
 ggplot() +
   geom_sf(data = m) +
   geom_sf(data = dplyr::select(d, geom)%>%dplyr::slice(1:15000) , col ='blue') +
-  geom_sf(data = dplyr::select(dd, geometry) , col ='red', alpha = 0) +
+  # geom_sf(data = dplyr::select(dd, geometry) , col ='red', alpha = 0) +
   theme_bw()
 
 
@@ -125,6 +125,225 @@ ggplot(output_long) +
   xlab('SNTemp Est. (C)')
 
 density()
+
+
+
+######################
+# figures for GLEON
+library(sf)
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+
+seg = sf::read_sf('20191002_Delaware_streamtemp/GIS/Segments_subset.shp') %>%
+  mutate(seg_id_nat = as.character(seg_id_nat))
+obs = readRDS('3_observations/in/obs_temp_full.rds')
+boundary = sf::read_sf('1_geo_boundary/in/DRB_Extent/DRB_Extent.shp')
+waterbodies = readRDS('2_1_model_fabric/out/nhd_waterbody_subset.rds') %>%
+  dplyr::filter(FType %in% c(463, 390))
+
+obs_agg = obs %>%
+  group_by(seg_id_nat) %>%
+  summarise(obs_dates = n()) %>%
+  ungroup()
+
+seg_obs = left_join(seg, obs_agg, by = "seg_id_nat") %>%
+  mutate(obs_dates = ifelse(is.na(obs_dates), 0, obs_dates),
+         obs_bins = cut(obs_dates, breaks = c(-1,0,10,100,1000,10000, 1000000000000),
+                                     labels = c('0','1-10','11-100','101-1000','1001-10000','>10001')))
+
+
+cols = viridis::viridis(n = 6, direction = -1)
+
+col_labs = c('0' = 'grey', '1-10' = cols[2],'11-100' = cols[3],
+             '101-1000' = cols[4],'1001-10000' = cols[5],'>10001' = cols[6])
+
+main_states = sf::st_as_sf(maps::map(database = 'state',
+                                     region = c('new york', 'new jersey', 'penn', 'delaware',
+                                                'maryland', 'west virginia','maine','virginia',
+                                                'ohio', 'vermont','new hampshire', 'massachusettes'),
+                                     fill = T, plot = F))
+us_map = sf::st_as_sf(maps::map(database = 'state', fill = T, plot = F, col = 'white', border = 'grey'))
+
+
+seg_plot = ggplot() +
+  geom_sf(data = main_states, fill = 'grey93', color = 'grey80') +
+  geom_sf(data = seg_obs, aes(col = obs_bins, fill = obs_bins), size = 1.1) +
+  geom_sf(data = boundary$geometry, col = 'black', alpha = 0) +
+  scale_color_manual(values = col_labs) +   scale_fill_manual(values = col_labs) +
+  theme_minimal()+
+  ylim(c(38.5,42.5))+
+  xlim(c(76.5,74)) +
+  guides(color=guide_legend(title="Days of observations"),fill=guide_legend(title="Days of observations"))
+
+seg_plot
+ggsave(filename = 'C:/Users/jzwart/Documents/Jake/Conferences/2019/GLEON/stream_obs_map.png',
+       plot = seg_plot, width = 6, height = 12, units = 'in', dpi = 400)
+
+sites_v_obs = seg_obs %>% select(obs_dates) %>% arrange(obs_dates) %>% mutate(seq_id = seq(1,nrow(.))) %>%
+  group_by(obs_dates) %>%
+  summarise(n_sites = max(seq_id)) %>%
+  ungroup() %>% mutate(n_sites = 457 - n_sites)
+
+obs_days = ggplot(dplyr::filter(sites_v_obs, obs_dates >0), aes(x = obs_dates, y = n_sites)) +
+  geom_line(size = 2) +
+  scale_x_log10() +
+  theme_classic() +
+  theme(axis.title = element_text(size = 26),
+        axis.text = element_text(size = 26)) +
+  ylab('Number of stream sites') +
+  xlab('Days of observations')
+
+obs_days
+ggsave(filename = 'C:/Users/jzwart/Documents/Jake/Conferences/2019/GLEON/stream_obs_days.png',
+       plot = obs_days, width = 8, height = 8, units = 'in', dpi = 400)
+
+
+map_of_us = ggplot() +
+  geom_sf(data = us_map, color = 'grey70', fill ='grey96') +
+  geom_sf(data = boundary$geometry, col = 'black', fill = 'blue') +
+  theme_minimal()
+
+map_of_us
+ggsave(filename = 'C:/Users/jzwart/Documents/Jake/Conferences/2019/GLEON/us_map.png',
+       plot = map_of_us, width = 8, height = 8, units = 'in', dpi = 400)
+
+lakes_sub = dplyr::filter(waterbodies, AreaSqKm >= 0.04)
+
+lakes_map = ggplot() +
+  geom_sf(data = main_states, fill = 'grey93', color = 'grey80') +
+  geom_sf(data = lakes_sub, color = 'blue', fill = 'lightblue') +
+  geom_sf(data = boundary$geometry, col = 'black', alpha = 0) +
+  theme_minimal()+
+  ylim(c(38.5,42.5))+
+  xlim(c(76.5,74))
+
+lakes_map
+ggsave(filename = 'C:/Users/jzwart/Documents/Jake/Conferences/2019/GLEON/lakes_map.png',
+       plot = lakes_map, width = 6, height = 12, units = 'in', dpi = 400)
+
+
+# combine NWIS and WQP inventories
+
+nwis_dv <- feather::read_feather('../2wp-temp-observations/2_nwis_pull/inout/nwis_dv_inventory.feather') %>%
+  select(site_id = site_no, latitude = dec_lat_va, longitude = dec_long_va)
+
+wqp <- feather::read_feather('../2wp-temp-observations/1_wqp_pull/inout/wqp_inventory.feather') %>%
+  select(site_id = MonitoringLocationIdentifier, latitude, longitude)
+
+nwis_uv <- feather::read_feather('../2wp-temp-observations/2_nwis_pull/inout/nwis_uv_inventory_reduced.feather') %>%
+  select(site_id = site_no, latitude = dec_lat_va, longitude = dec_long_va)
+
+sites <- bind_rows(nwis_dv, wqp, nwis_uv) %>%
+  distinct()
+
+
+collapsed_dat <- readRDS('../2wp-temp-observations/5_data_munge/out/daily_temperatures.rds') %>%
+  left_join(sites) %>%
+  group_by(site_id, latitude, longitude) %>%
+  summarize(count = n()) %>%
+  filter(!latitude > 49.3457868, !latitude < 24.7433195, !longitude < -124.7844079, !longitude > -66.9513812)
+
+
+obs_site_points <- purrr::map2(collapsed_dat$longitude, collapsed_dat$latitude, function(latitude, longitude) {
+  st_point(c(latitude, longitude), dim='XY')
+})
+collapsed_dat_sf <- collapsed_dat %>%
+  st_set_geometry(st_sfc(obs_site_points)) %>%
+  st_set_crs(4326) %>%
+  st_transform(crs=st_crs(boundary$geometry))
+
+drb_sites = collapsed_dat_sf[st_intersects(boundary, collapsed_dat_sf)[[1]], ]
+
+drb_sites_trans = drb_sites %>%
+  st_transform(crs=st_crs(lakes_sub$geom))
+
+lakes_out = lakes_sub %>%
+  mutate(obs_days = 0)
+for(i in 1:length(lakes_out$Permanent_Identifier)){
+
+  cur_lake = drb_sites_trans[st_intersects(lakes_out[i,], drb_sites_trans)[[1]], ]
+  if(length(cur_lake$site_id) == 0){
+    next
+  }else{
+    lakes_out$obs_days[i] = sum(cur_lake$count)
+  }
+}
+
+lakes_obs = mutate(lakes_out, obs_bins = cut(obs_days, breaks = c(-1,0,10,100,1000,10000, 1000000000000),
+                      labels = c('0','1-10','11-100','101-1000','1001-10000','>10001')))
+
+cols = viridis::viridis(n = 6, direction = -1)
+
+col_labs = c('0' = 'grey', '1-10' = cols[2],'11-100' = cols[3],
+             '101-1000' = cols[4],'1001-10000' = cols[5],'>10001' = cols[6])
+
+lakes_plot = ggplot() +
+  geom_sf(data = main_states, fill = 'grey93', color = 'grey80') +
+  geom_sf(data = lakes_obs, aes(col = obs_bins, fill = obs_bins)) +
+  geom_sf(data = boundary$geometry, col = 'black', alpha = 0) +
+  scale_color_manual(values = col_labs) +   scale_fill_manual(values = col_labs) +
+  theme_minimal()+
+  ylim(c(38.5,42.5))+
+  xlim(c(76.5,74)) +
+  guides(color=guide_legend(title="Days of observations"),fill=guide_legend(title="Days of observations"))
+
+lakes_plot
+ggsave(filename = 'C:/Users/jzwart/Documents/Jake/Conferences/2019/GLEON/lake_obs_map.png',
+       plot = lakes_plot, width = 6, height = 12, units = 'in', dpi = 400)
+
+
+lake_sites_v_obs = lakes_obs %>% select(obs_days) %>% arrange(obs_days) %>% mutate(seq_id = seq(1,nrow(.))) %>%
+  group_by(obs_days) %>%
+  summarise(n_sites = max(seq_id)) %>%
+  ungroup() %>% mutate(n_sites = nrow(lakes_obs) - n_sites)
+
+lake_obs_days = ggplot(dplyr::filter(lake_sites_v_obs, obs_days >0), aes(x = obs_days, y = n_sites)) +
+  geom_line(size = 2) +
+  geom_line(data= dplyr::filter(sites_v_obs, obs_dates >0), aes(x = obs_dates, y = n_sites), size = 2, col= 'grey') +
+  scale_x_log10() +
+  theme_classic() +
+  theme(axis.title = element_text(size = 26),
+        axis.text = element_text(size = 26)) +
+  ylab('Number of sites') +
+  xlab('Days of observations')
+
+lake_obs_days
+ggsave(filename = 'C:/Users/jzwart/Documents/Jake/Conferences/2019/GLEON/lake_obs_days.png',
+       plot = lake_obs_days, width = 8, height = 8, units = 'in', dpi = 400)
+
+
+
+#example covariance matrix
+
+covary_dat <- readRDS('../2wp-temp-observations/5_data_munge/out/daily_temperatures.rds') %>%
+  dplyr::filter(site_id %in% drb_sites$site_id)
+
+sites_summary = covary_dat %>%
+  group_by(site_id) %>%
+  summarise(obs_days = n()) %>% ungroup() %>% arrange(desc(obs_days))
+
+sites = sites_summary$site_id[sample(x = 1:100, size = 40, replace = F)]
+
+covary_dat_sub = dplyr::filter(covary_dat, site_id %in% sites) %>%
+  spread(key = site_id, value = temp_degC)
+
+cor_mat = matrix(NA,ncol = 40,nrow = 40)
+for(i in 1:40){
+  for(j in 1:40){
+    cur_dat = na.omit(cbind(covary_dat_sub[,(i+3)], covary_dat_sub[,(j+3)]))
+
+    cur_cor = cor(cur_dat[,1], cur_dat[,2])
+    cor_mat[i,j] = cur_cor
+  }
+}
+
+cor_mat = ifelse(is.na(cor_mat), 0, cor_mat)
+
+library(plot.matrix)
+
+plot(cor_mat, col = topo.colors)
+
 
 
 
