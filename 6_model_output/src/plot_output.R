aa97066b46b0eb199c88aa398ff0e69ae24fcea8
jzwart <jzwart@usgs.gov>
2019-12-16 10:24:40 -0500

    plotting output

--- 6_model_output/src/plot_output.R
+++ 6_model_output/src/plot_output.R
@@ -11,13 +11,13 @@ n_step = length(d$dates)
 Y_no_assim = dd$Y
 
 obs[,1,1]
-site = 439
-plot(Y[site,,1], type = 'l',ylim =  range(c(Y[site,,], obs[site,1,]), na.rm = T))
+site = 377
+plot(Y[site,,1], type = 'l',ylim =  range(c(Y[site,,], obs[site,1,]), na.rm = T), ylab = 'Stream Temp (C)', xlab = 'Time Step')
 for(i in 1:n_en){
   lines(Y_no_assim[site,,i], col = 'grey')
   lines(Y[site,,i])
 }
-points(obs[site,1,], col = 'red')
+points(obs[site,1,], col = 'red', pch = 16, cex = 1.2)
 arrows(1:n_step, obs[site,1,]+R[site,site,], 1:n_step, obs[site,1,]-R[site,site,],
        angle = 90, length = .05, col = 'red', code = 3)
 
@@ -33,17 +33,21 @@ for(i in 1:n_en){
 uncert = c()
 uncert_no_assim = c()
 n_obs = c()
+ss_tau = c()
+gw_tau = c()
 for(site in 1:456){
   # uncertainty at end of run
   uncert = rbind(uncert, sd(Y[site,n_step,]))
-  # uncert_no_assim = rbind(uncert_no_assim, sd(Y_no_assim[site, n_step, ]))
+  uncert_no_assim = rbind(uncert_no_assim, sd(Y_no_assim[site, n_step, ]))
   n_obs = rbind(n_obs, sum(!is.na(obs[site,1,])))
+  ss_tau = rbind(ss_tau, mean(Y[(456*1 + site), n_step, ]))
+  gw_tau = rbind(gw_tau, mean(Y[(456*2 + site), n_step, ]))
 }
 
-plot(uncert ~ n_obs)
+plot(uncert ~ n_obs, ylab = 'Temperature Standard Deviation', xlab= '# of Observations Assimilated', pch =16, cex = 1.5)
 
-loc = cbind(d$model_locations, uncert,uncert_no_assim, n_obs)
-loc = cbind(d$model_locations, uncert, n_obs)
+loc = cbind(d$model_locations, uncert,uncert_no_assim, n_obs, ss_tau, gw_tau)
+# loc = cbind(d$model_locations, uncert, n_obs)
 
 model_fabric = sf::read_sf('20191002_Delaware_streamtemp/GIS/Segments_subset.shp') %>%
   mutate(seg_id_nat = as.character(seg_id_nat),
@@ -56,12 +60,13 @@ windows()
 ggplot() +
   geom_sf(data = loc, aes(color = log10(uncert)))+
   scale_color_viridis_c() +
-  theme_minimal()
+  theme_minimal()+
+  theme(legend.title = element_text('Temp SD'))
 
 # reduction in uncertainty
 windows()
 ggplot() +
-  geom_sf(data = loc, aes(color = (uncert_no_assim - uncert)))+
+  geom_sf(data = loc, aes(color = (uncert_no_assim - uncert), size = .1*(uncert_no_assim - uncert)))+
   scale_color_viridis_c(direction = -1) +
   theme_minimal()
 
@@ -74,7 +79,23 @@ ggplot() +
 loc$red_uncert = loc$uncert_no_assim - loc$uncert
 windows()
 plot(loc$red_uncert ~ loc$n_obs)
+abline(lm(loc$red_uncert~loc$n_obs))
 
+windows()
+loc$ss_tau = loc$ss_tau + 40
+ggplot() +
+  geom_sf(data = loc, aes(color = ss_tau ))+
+  scale_color_viridis_c() +
+  theme_minimal()+
+  theme(legend.title = element_text('ss_tau'))
+
+
+windows()
+ggplot() +
+  geom_sf(data = loc, aes(color = log10(uncert)))+
+  scale_color_viridis_c() +
+  theme_minimal()+
+  theme(legend.title = element_text('Temp SD'))
 
 # RMSE
 time_period = 1:n_step
@@ -108,13 +129,13 @@ ggplot() +
 
 windows()
 ggplot() +
-  geom_sf(data = dplyr::filter(loc, !is.na(reduction_rmse)), aes(color = reduction_rmse))+
+  geom_sf(data = dplyr::filter(loc, !is.na(reduction_rmse)), aes(color = reduction_rmse,size = abs(reduction_rmse)))+
   # scale_color_distiller(palette = 'RdBu', limits = c(-1,1)*max(abs(loc$reduction_rmse))) +
   scale_color_gradient2() +
   theme_minimal()+
   geom_sf(data = dplyr::filter(loc, is.na(reduction_rmse)), color = 'grey90')
 
-plot(loc$reduction_rmse ~ loc$n_obs)
+plot(loc$reduction_rmse ~ loc$n_obs, ylab ='Reduction in RMSE', xlab = '# of Obs Assimilated', pch =16, cex = 1.5)
 
 sum(loc$reduction_rmse/ length(!is.na(loc$n_obs)), na.rm = T)
 
