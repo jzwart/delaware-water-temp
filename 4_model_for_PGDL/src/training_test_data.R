be6c05127d8de040beff86b20231a3b73eee01a1
jzwart <jzwart@usgs.gov>
2020-01-16 14:37:34 -0500

    building synthetic data training set

---
+++ 4_model_for_PGDL/src/training_test_data.R
@@ -0,0 +1,83 @@
+
+#' @param ind_file indicator file
+#' @param data_file SNTemp output data file
+#' @param percent_sites how many stream segments to use for training
+#' @param temporal_res temporal resolution of the sampling at each site (days)
+#' @param obs_error observation error added onto the SNTemp output
+#' @param obs_type is the observation from in-situ or remote sensing? RS obs occur on the same day
+#' @param exp_n number of experiments using this scheme
+#' @param test_yrs how many years at the end of time series to leave as testing only
+build_synthetic_training <- function(ind_file,
+                                     data_file,
+                                     percent_sites,
+                                     temporal_res,
+                                     obs_error,
+                                     obs_type = 'in_situ',
+                                     exp_n,
+                                     test_yrs,
+                                     gd_config = 'lib/cfg/gd_config.yml'){
+
+  all_data = feather::read_feather(data_file)
+
+  dates = all_data %>% pull(date) %>% unique
+
+  segs = all_data %>% pull(seg_id_nat) %>% unique
+
+  n_dates = length(dates)
+  n_segs_obs = round(length(segs) * percent_sites / 100)
+
+  exps = 1:exp_n
+
+  if(obs_type == 'in_situ'){
+    for(i in exps){
+      set.seed(42 + as.numeric(i))
+
+      segs_observed = sample(x = segs, size = n_segs_obs, replace = F)
+
+      cur_train_test = all_data %>%
+        mutate(train_test = 'test') %>%
+        group_by(seg_id_nat) %>%
+        # randomize starting date of sampling
+        mutate(train_test = case_when(date %in% dates[seq(sample(1:temporal_res,1), n_dates, by = temporal_res)] &
+                                        seg_id_nat %in% segs_observed ~ 'train',
+                                      TRUE ~ 'test')) %>%
+        ungroup() %>%
+        mutate(train_test = case_when(date > (max(all_data$date) - as.numeric(test_yrs)*365) ~ 'test',
+                                      TRUE ~ train_test)) %>%
+        pull(train_test)
+
+      col_name = paste0('exp',i) %>% noquote()
+      all_data = mutate(all_data, !!col_name := cur_train_test) %>%
+        select(seg_id_nat, date, starts_with('exp')) # only keeping id, date, and experiments to cut down on redundant data
+    }
+  }else if(obs_type == 'rs'){
+    for(i in exps){
+      set.seed(42 + as.numeric(i))
+
+      segs_observed = sample(x = segs, size = n_segs_obs, replace = F)
+      dates_observed = dates[seq(sample(1:temporal_res,1), n_dates, by = temporal_res)]
+
+      cur_train_test = all_data %>%
+        mutate(train_test = 'test') %>%
+        group_by(seg_id_nat) %>%
+        # randomize starting date of sampling
+        mutate(train_test = case_when(date %in% dates_observed &
+                                        seg_id_nat %in% segs_observed ~ 'train',
+                                      TRUE ~ 'test')) %>%
+        ungroup() %>%
+        mutate(train_test = case_when(date > (max(all_data$date) - as.numeric(test_yrs)*365) ~ 'test',
+                                      TRUE ~ train_test)) %>%
+        pull(train_test)
+
+      col_name = paste0('exp',i) %>% noquote()
+      all_data = mutate(all_data, !!col_name := cur_train_test) %>%
+        select(seg_id_nat, date, starts_with('exp')) # only keeping id, date, and experiments to cut down on redundant data
+    }
+  }
+
+
+  out_file = as_data_file(ind_file)
+  feather::write_feather(x = all_data, path = out_file)
+  gd_put(remote_ind = ind_file, local_source = out_file, config_file = gd_config)
+}
+
