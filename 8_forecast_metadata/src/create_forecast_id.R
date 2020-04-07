6f8bee14986fc788d11360b3928efe16c200ef95
jzwart <jzwart@usgs.gov>
2020-04-07 11:20:39 -0400

    rebuild

--- 8_forecast_metadata/src/create_forecast_id.R
+++ 8_forecast_metadata/src/create_forecast_id.R
@@ -3,6 +3,6 @@
 # this should be created every time a new forecast is generated
 create_forecast_id <- function(forecast_project_id){
 
-  Forecast_id = paste0(forecast_project_id$forecast_project_id, uuid::UUIDgenerate())
+  Forecast_id = paste(forecast_project_id$forecast_project_id, uuid::UUIDgenerate(), sep = '_')
   return(Forecast_id)
 }
