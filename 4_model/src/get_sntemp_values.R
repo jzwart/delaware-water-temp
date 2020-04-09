300d0e305450e914ce18d3bebe7ba5c6d0caf30b
jzwart <jzwart@usgs.gov>
2020-04-09 17:14:33 -0400

    updating sntemp run through 2019

--- 4_model/src/get_sntemp_values.R
+++ 4_model/src/get_sntemp_values.R
@@ -41,9 +41,23 @@ get_sntemp_temperature = function(model_output_file, model_fabric_file){
 
 get_sntemp_intermediates = function(model_output_file, model_fabric_file){
 
-  model_output = read.table(model_output_file, header = T, stringsAsFactors = F) %>%
-    dplyr::slice(-1) # first row indicates column type
+  ### This was given error with updated model output and reading in the data
+  # error was:
+  #  Error in scan(file = file, what = what, sep = sep, quote = quote, dec = dec,  :
+  #       line 1489316 did not have 20 elements
+  # even though all rows had 20 elements from manual inspection of R-indicated problem rows
+  # model_output = read.table(model_output_file, header = T, stringsAsFactors = F) %>%
+  #    dplyr::slice(-1) # first row indicates column type
 
+  # using fread to solve error documented above ^
+  model_output = data.table::fread(file = model_output_file,
+                                   skip = 24, header = F)
+  # head(model_output)
+
+  cols = readr::read_delim(file = model_output_file, delim = '\t', n_max = 2, skip = 24)
+
+  colnames(model_output) = colnames(cols)
+  rm(cols)
   # model_otuput = read.csv(model_output_file,sep = ' ', skip = 20, header = T, stringsAsFactors = F)
   #
   # fc = file(file.path(model_output_file))
