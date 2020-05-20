f474074eb5f5077d3060afb3c9430f4452e2bf00
jzwart <jzwart@usgs.gov>
2020-05-20 16:59:07 -0400

    updating gather sntemp output functions

--- 4_model/src/set_sntemp_output.R
+++ 4_model/src/set_sntemp_output.R
@@ -1,8 +1,10 @@
 
 
-set_sntemp_output = function(output_names, control_file = 'delaware.control'){
+set_sntemp_output = function(output_names,
+                             model_run_loc,
+                             control_file = 'delaware.control'){
 
-  ctrl = readLines(file.path('20191002_Delaware_streamtemp/control', control_file))
+  ctrl = readLines(file.path(model_run_loc, 'control', control_file))
 
   # location of previous output variables
   output_loc_start = grep('aniOutVar_names', ctrl) + 3
@@ -30,7 +32,7 @@ set_sntemp_output = function(output_names, control_file = 'delaware.control'){
     new_ctrl = c(new_ctrl[1:(new_output_loc_start-1)], output_names$sntemp_output, new_ctrl[new_output_loc_start:length(new_ctrl)])
   }
 
-  writeLines(new_ctrl, file.path('20191002_Delaware_streamtemp/control', control_file))
+  writeLines(new_ctrl, file.path(model_run_loc, 'control', control_file))
 }
 
 
