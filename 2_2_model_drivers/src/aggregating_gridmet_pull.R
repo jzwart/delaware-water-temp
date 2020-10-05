f22647e0416d890da8ed3f83dabcd2cb1dbc1373
jzwart <jzwart@usgs.gov>
2020-10-05 09:51:28 -0400

    rebuilding uncalibrated output with gridmet drivers

---
+++ 2_2_model_drivers/src/aggregating_gridmet_pull.R
@@ -0,0 +1,135 @@
+
+
+# aggregating gridmet data pull and adding to model run loc
+# tmax
+append_new_driver_data(orig_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/8084_modelidx_tmax.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/8589_modelidx_tmax.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'tmax')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/tmax.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/9094_modelidx_tmax.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'tmax')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/tmax.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/9599_modelidx_tmax.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'tmax')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/tmax.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/0004_modelidx_tmax.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'tmax')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/tmax.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/0509_modelidx_tmax.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'tmax')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/tmax.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/1014_modelidx_tmax.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'tmax')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/tmax.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/1519_modelidx_tmax.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'tmax')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/tmax.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/20_modelidx_tmax.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'tmax')
+
+
+
+# tmin
+append_new_driver_data(orig_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/8084_modelidx_tmin.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/8589_modelidx_tmin.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'tmin')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/tmin.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/9094_modelidx_tmin.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'tmin')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/tmin.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/9599_modelidx_tmin.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'tmin')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/tmin.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/0004_modelidx_tmin.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'tmin')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/tmin.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/0509_modelidx_tmin.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'tmin')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/tmin.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/1014_modelidx_tmin.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'tmin')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/tmin.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/1519_modelidx_tmin.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'tmin')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/tmin.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/20_modelidx_tmin.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'tmin')
+
+
+# prcp
+append_new_driver_data(orig_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/8084_modelidx_prcp.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/8589_modelidx_prcp.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'prcp')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/prcp.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/9094_modelidx_prcp.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'prcp')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/prcp.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/9599_modelidx_prcp.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'prcp')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/prcp.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/0004_modelidx_prcp.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'prcp')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/prcp.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/0509_modelidx_prcp.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'prcp')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/prcp.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/1014_modelidx_prcp.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'prcp')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/prcp.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/1519_modelidx_prcp.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'prcp')
+
+append_new_driver_data(orig_driver_file = '20191002_Delaware_streamtemp/input/prcp.cbh',
+                       new_driver_file = '2_2_model_drivers/in/Sept2020_gridMETpull/20_modelidx_prcp.cbh',
+                       model_run_loc = '20191002_Delaware_streamtemp',
+                       driver_name = 'prcp')
+
+
+
+# need to update sf_data after updating driver dates, otherwise model won't run
+update_sf_data(
+  new_driver_file = '20191002_Delaware_streamtemp/input/tmax.cbh',
+  model_run_loc = I('20191002_Delaware_streamtemp'))
---
+++ 2_2_model_drivers/src/comparing_daymet_gridmet_runs.R
@@ -0,0 +1,108 @@
+
+
+# comparing PRMS-SNTemp output from original DayMet drivers and new GridMet drivers pulled in Sept 2020
+
+source('4_model/src/run_sntemp.R')
+source('4_model/src/get_sntemp_values.R')
+
+start = '1980-01-01'
+stop = '2016-01-01'
+restart = 'F'
+spinup = 'F'
+
+# run prms-sntemp with daymet drivers
+run_sntemp(start = start,
+           stop = stop,
+           spinup = spinup,
+           restart = restart,
+           model_run_loc = '20191002_Delaware_streamtemp',
+           precip_file = './input/Daymet/prcp.cbh',
+           tmax_file = './input/Daymet/tmax.cbh',
+           tmin_file = './input/Daymet/tmin.cbh',
+           sf_data_file = './input/Daymet/sf_data') # original model loc has daymet drivers
+
+daymet_temp = get_sntemp_temperature(model_output_file = '20191002_Delaware_streamtemp/output/seg_tave_water.csv',
+                                    model_fabric_file = '20191002_Delaware_streamtemp/GIS/Segments_subset.shp')
+daymet_flow = get_sntemp_discharge(model_output_file = '20191002_Delaware_streamtemp/output/seg_outflow.csv',
+                                  model_fabric_file = '20191002_Delaware_streamtemp/GIS/Segments_subset.shp')
+
+# run prms-sntemp with gridmet drivers
+run_sntemp(start = start,
+           stop = stop,
+           spinup = spinup,
+           restart = restart,
+           model_run_loc = '20191002_Delaware_streamtemp') # original model loc has daymet drivers
+
+gridmet_temp = get_sntemp_temperature(model_output_file = '20191002_Delaware_streamtemp/output/seg_tave_water.csv',
+                                     model_fabric_file = '20191002_Delaware_streamtemp/GIS/Segments_subset.shp')
+gridmet_flow = get_sntemp_discharge(model_output_file = '20191002_Delaware_streamtemp/output/seg_outflow.csv',
+                                   model_fabric_file = '20191002_Delaware_streamtemp/GIS/Segments_subset.shp')
+
+
+library(dplyr)
+library(ggplot2)
+cfs_to_cms = 1/(3.28084^3)
+compare_temp = left_join(daymet_temp, gridmet_temp,
+                         by = c('seg_id_nat', 'model_idx', 'date'),
+                         suffix = c('_daymet', '_gridmet'))
+compare_flow = left_join(daymet_flow, gridmet_flow,
+                         by = c('seg_id_nat', 'model_idx', 'date'),
+                         suffix = c('_daymet', '_gridmet'))
+
+caret::RMSE(pred = compare_temp$water_temp_gridmet, obs = compare_temp$water_temp_daymet)
+
+caret::RMSE(pred = compare_flow$discharge_gridmet, obs = compare_flow$discharge_daymet) * cfs_to_cms
+
+
+d = filter(compare_temp, model_idx == '1')
+windows()
+ggplot(d, aes(x = date, y = water_temp_gridmet, color = 'Gridmet')) +
+  geom_line() +
+  geom_line(data = d, aes(x = date, y = water_temp_daymet, color = 'Daymet')) +
+  theme_minimal() + ylab('Water Temp (C)')
+ggplot(d, aes(x = water_temp_daymet, y = water_temp_gridmet)) +
+  geom_point() +
+  theme_minimal() +
+  ylab('Gridmet Water Temp (C)') + xlab('Daymet Water Temp (C)')+
+  geom_abline(color = 'red')
+caret::RMSE(pred = d$water_temp_gridmet, obs = d$water_temp_daymet)
+
+temp_summary = compare_temp %>%
+  group_by(model_idx) %>%
+  summarise(rmse = caret::RMSE(pred = water_temp_gridmet, obs = water_temp_daymet)) %>%
+  ungroup()
+ggplot(temp_summary) + geom_boxplot(aes(y = rmse), size =2) + ylim(c(0,2)) + theme_minimal()+ ylab('Temp RMSE')
+
+
+
+d = filter(compare_flow, model_idx == '25')
+windows()
+ggplot(d, aes(x = date, y = discharge_gridmet * cfs_to_cms, color = 'Gridmet')) +
+  geom_line() +
+  geom_line(data = d, aes(x = date, y = discharge_daymet * cfs_to_cms, color = 'Daymet')) +
+  theme_minimal() + ylab('Discharge (CMS)')
+ggplot(d, aes(x = discharge_daymet * cfs_to_cms, y = discharge_gridmet * cfs_to_cms)) +
+  geom_point() +
+  theme_minimal() +
+  ylab('Gridmet Discharge (CMS)') + xlab('Daymet Discharge (CMS)')+
+  geom_abline(color = 'red') + scale_y_log10() + scale_x_log10()
+caret::RMSE(pred = d$discharge_daymet, obs = d$discharge_gridmet) * cfs_to_cms
+
+flow_summary = compare_flow %>%
+  group_by(model_idx) %>%
+  summarise(rmse = caret::RMSE(pred = discharge_daymet, obs = discharge_gridmet) * cfs_to_cms,
+            mean_discharge = mean(discharge_daymet) * cfs_to_cms,
+            standardized_rmse = rmse / mean_discharge) %>%
+  ungroup()
+ggplot(flow_summary) + geom_boxplot(aes(y = rmse), size =2) +
+  #ylim(c(0,2)) +
+  theme_minimal()+ ylab('Flow RMSE (CMS)') + scale_y_log10()
+ggplot(flow_summary) + geom_boxplot(aes(y = standardized_rmse), size =2) +
+  #ylim(c(0,2)) +
+  theme_minimal()+ ylab('Flow RMSE (CMS) / Mean Flow (CMS)')
+ggplot(flow_summary, aes(x = mean_discharge, y = standardized_rmse)) +
+  geom_point(size =2) +
+  theme_minimal()+
+  ylab('Flow RMSE (CMS) / Mean Flow (CMS)') +
+  xlab('Mean Discharge (CMS)') + scale_x_log10()
+
